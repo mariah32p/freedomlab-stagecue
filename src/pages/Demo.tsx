b9d586f4f1fc9aa87d4950ed64161a240ae203dc
mariah32p <mariah@32parallels.com>
2025-08-30 23:06:07 -0500

    Add missing button closing tag

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1303,6 +1303,7 @@ export function Demo() {
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
               </svg>
+            </button>
             </div>
           </div>
         </div>
