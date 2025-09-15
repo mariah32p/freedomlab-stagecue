d73e7613b5d236d45ce8cd161f8bfa9deee7ea1c
mariah32p <mariah@32parallels.com>
2025-09-15 17:54:26 -0500

    Updated SpeakerPortal.tsx

--- src/pages/SpeakerPortal.tsx
+++ src/pages/SpeakerPortal.tsx
@@ -222,6 +222,12 @@ export function SpeakerPortal() {
                     <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                     </svg>
+                    Error saving
+                  </div>
+                )}
+              </div>
+            </div>
+            
             <div className="space-y-4">
               <textarea
                 className="w-full h-96 p-4 border border-slate-300 rounded-lg resize-none focus:ring-2 focus:ring-teal-500 focus:border-teal-500 text-base leading-relaxed"
